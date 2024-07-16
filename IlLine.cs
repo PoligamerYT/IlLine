b88f25b7b52de05d7ac2178354d173ab95d38900
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2024-07-16 15:55:42 -0400

    rebase off of bepinex plugin template
    - swaps to sdk style csproj
    - lowers target framework to net framework 3.5 for compatibility with more games
    - targets lowest version of unity on bepinex nuget (3.4.0) for better compatibility with older games

--- IlLine.cs
+++ IlLine.cs
@@ -1,96 +1,91 @@
-﻿using BepInEx;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
-using Mono.Cecil.Pdb;
-using MonoMod.Cil;
-using MonoMod.RuntimeDetour;
+﻿#region
+
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using BepInEx;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Pdb;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 
-namespace IlLine
+#endregion
+
+namespace IlLine;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class IlLine : BaseUnityPlugin
 {
-    [BepInPlugin("com.poligamer.IlLine", "IlLine", "1.0.0")]
-    public class IlLine : BaseUnityPlugin
+    private IlLine()
     {
-        private IlLine()
-        {
-            new ILHook(typeof(StackTrace).GetMethod("AddFrames", BindingFlags.Instance | BindingFlags.NonPublic), new ILContext.Manipulator(this.IlHook));
-        }
-        private void IlHook(ILContext il)
-        {
-            ILCursor ilcursor = new ILCursor(il);
-            ILCursor ilcursor2 = ilcursor;
-            Func<Instruction, bool>[] array = new Func<Instruction, bool>[1];
-            array[0] = ((Instruction x) => ILPatternMatchingExt.MatchCallvirt(x, typeof(StackFrame).GetMethod("GetFileLineNumber", BindingFlags.Instance | BindingFlags.Public)));
-            ilcursor2.GotoNext(array);
-            ilcursor.RemoveRange(2);
-            ilcursor.EmitDelegate<Func<StackFrame, string>>(new Func<StackFrame, string>(IlLine.GetLineOrIL));
-        }
+        new ILHook(typeof(StackTrace).GetMethod("AddFrames", BindingFlags.Instance | BindingFlags.NonPublic), IlHook);
+    }
 
-        private static string GetLineOrIL(StackFrame instace)
+    private void IlHook(ILContext il)
+    {
+        var ilcursor = new ILCursor(il);
+        var ilcursor2 = ilcursor;
+        var array = new Func<Instruction, bool>[1];
+        array[0] = x =>
+            x.MatchCallvirt(typeof(StackFrame).GetMethod("GetFileLineNumber", BindingFlags.Instance | BindingFlags.Public));
+        ilcursor2.GotoNext(array);
+        ilcursor.RemoveRange(2);
+        ilcursor.EmitDelegate(GetLineOrIL);
+    }
+
+    private static string GetLineOrIL(StackFrame instance)
+    {
+        var dllPath = instance.GetMethod().DeclaringType.Assembly.Location;
+        var pdbPath = Path.ChangeExtension(dllPath, "pdb");
+        var fileLineNumber = instance.GetFileLineNumber();
+        if (fileLineNumber == -1 || fileLineNumber == 0)
         {
-            string dllPath = instace.GetMethod().DeclaringType.Assembly.Location;
-            string pdbPath = Path.ChangeExtension(dllPath, "pdb");
-            int fileLineNumber = instace.GetFileLineNumber();
-            if (fileLineNumber == -1 || fileLineNumber == 0)
+            if (File.Exists(dllPath))
             {
-                if (File.Exists(dllPath))
+                if (File.Exists(pdbPath))
                 {
-                    if(File.Exists(pdbPath)) 
-                    {
-                        return GetLineFromPBD(instace.GetILOffset(), dllPath, pdbPath);
-                    }
-                    else
-                    {
-                        return "IL_" + instace.GetILOffset().ToString("X4");
-                    }
+                    return GetLineFromPBD(instance.GetILOffset(), dllPath, pdbPath);
                 }
-                else
-                {
-                    return "IL_" + instace.GetILOffset().ToString("X4");
-                }
+                return "IL_" + instance.GetILOffset().ToString("X4");
             }
-            return fileLineNumber.ToString();
+            return "IL_" + instance.GetILOffset().ToString("X4");
         }
+        return fileLineNumber.ToString();
+    }
 
-        private static string GetLineFromPBD(int ilOffset, string dllPath, string pdbPath)
+    private static string GetLineFromPBD(int ilOffset, string dllPath, string pdbPath)
+    {
+        using (var assemblyDefinition = AssemblyDefinition.ReadAssembly(dllPath, new ReaderParameters
+               {
+                   ReadSymbols = true,
+                   SymbolReaderProvider = new PdbReaderProvider(),
+                   SymbolStream = new FileStream(pdbPath, FileMode.Open, FileAccess.Read)
+               }))
         {
-            using (var assemblyDefinition = AssemblyDefinition.ReadAssembly(dllPath, new ReaderParameters
+            foreach (var module in assemblyDefinition.Modules)
             {
-                ReadSymbols = true, 
-                SymbolReaderProvider = new PdbReaderProvider(),
-                SymbolStream = new FileStream(pdbPath, FileMode.Open, FileAccess.Read)
-            }))
-            {
-                foreach (var module in assemblyDefinition.Modules)
+                foreach (var method in module.GetTypes().SelectMany(t => t.Methods))
                 {
-                    foreach (var method in module.GetTypes().SelectMany(t => t.Methods))
+                    foreach (var instruction in method.Body.Instructions)
                     {
-                        foreach (var instruction in method.Body.Instructions)
+                        if (instruction.Offset == ilOffset)
                         {
-                            if (instruction.Offset == ilOffset)
+                            var sequencePoint = method.DebugInformation.GetSequencePoint(instruction);
+                            if (sequencePoint != null)
                             {
-                                var sequencePoint = method.DebugInformation.GetSequencePoint(instruction);
-                                if (sequencePoint != null)
-                                {
-                                    int sourceLine = sequencePoint.StartLine;
+                                var sourceLine = sequencePoint.StartLine;
 
-                                    return sourceLine.ToString();
-                                }
+                                return sourceLine.ToString();
                             }
                         }
                     }
                 }
             }
-
-            return "IL_" + ilOffset.ToString("X4");
         }
 
-        internal const string modname = "IlLine";
-
-        internal const string version = "1.0.0";
+        return "IL_" + ilOffset.ToString("X4");
     }
 }
